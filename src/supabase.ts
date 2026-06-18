5ccd35e65df9d394405b3905dc26217054c38303
audreynunes-arch <audrey.nunes@live.fr>
2026-06-18 15:15:08 +0200

    supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://uvubrbxcvpvdxtobrfjj.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_kBwTwoJ7eKzhh2SO1auMsA_9kkClbMF';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2dWJyYnhjdnB2ZHh0b2JyZmpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NzIwNTMsImV4cCI6MjA5MjM0ODA1M30.CxZylEw-72aLyeu7Z2ZUTTU89FJ4LUm5rwjIvW5IUM0';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
